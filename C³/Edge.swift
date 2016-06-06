c7ac7fcede731ee143aab70bdb071f95c0072ee9
Kota Nakano <kotan.kn@me.com>
2016-06-06 14:21:17 +0900

    push

---
+++ C³/Edge.swift
@@ -0,0 +1,45 @@
+//
+//  Edge.swift
+//  iOS
+//
+//  Created by Kota Nakano on 6/1/16.
+//
+//
+
+import CoreData
+import Metal
+
+class Edge: NSManagedObject {
+	@NSManaged var gain: NSData
+	@NSManaged var input: Cell
+	@NSManaged var output: Cell
+	class MTLRef {
+		var gain: MTLBuffer?
+	}
+	let mtl: MTLRef = MTLRef()
+}
+extension Edge {
+	override func awakeFromFetch() {
+		
+	}
+}
+extension Edge: Network {
+	func clear ( ) {
+		input.clear()
+	}
+	func chain ( let callback: ( Cell -> Void ) ) {
+		input.chain( callback )
+	}
+	func train ( let eps: Float ) {
+		input.train( eps )
+	}
+}
+extension Edge: CoreDataSharedMetal {
+	func setup() {
+		print("via")
+		let mtlgain: MTLBuffer = context.allocate(data: gain)
+		print("dst")
+		gain = NSData(bytesNoCopy: mtlgain.contents(), length: mtlgain.length, freeWhenDone: false)
+		mtl.gain = mtlgain
+	}
+}
