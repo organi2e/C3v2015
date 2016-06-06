fece108cb2632e607f6529cee16f5a0b6bab79e7
Kota Nakano <kotan.kn@me.com>
2016-06-06 15:37:06 +0900

    テストコード追加・common.metal追加 organi2e版からそのうち移植

---
+++ C³/common.metal
@@ -0,0 +1,12 @@
+//
+//  common.metal
+//  C³
+//
+//  Created by Kota Nakano on 6/6/16.
+//
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+constant uint FIRED = 1 << 0;
--- Mac/C³Tests/C³Tests.swift
+++ Mac/C³Tests/C³Tests.swift
@@ -7,13 +7,28 @@
 //
 
 import XCTest
-@testable import C_
+//@testable
+import C3
 
 class C_Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let
+				I: Cell = context.newCell(width: 16, label: "I"),
+				H: Cell = context.newCell(width: 16, label: "H", input: [I]),
+				_: Cell = context.newCell(width: 16, label: "G", input: [H]) {
+				print("created")
+				context.join()
+				try context.store()
+			}
+		} catch let e {
+			print(e)
+		}
     }
     
     override func tearDown() {
@@ -24,13 +39,28 @@ class C_Tests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let O: Cell = context.searchCell(label: "G").first {
+				O.clear()
+				O.chain {
+					print("\($0.label) traced")
+				}
+				O.correct([], eps: 0.0)
+			}
+			context.join()
+			
+		} catch let e {
+			print(e)
+		}
     }
-    
+    /*
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+    */
 }
