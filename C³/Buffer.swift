1b9ebb67bd5eba9ab6ffc1a91e1b1cbfc4b49fe3
Kota Nakano <Kota@lab-n678.team-lab.local>
2016-06-07 11:45:45 +0900

    gpu/cpu行列積gemvの実装及びテストコード

--- C³/Buffer.swift
+++ C³/Buffer.swift
@@ -12,6 +12,7 @@ import simd
 internal struct Buffer {
 	let mtl: MTLBuffer?
 	let raw: NSData
+	let stream: UnsafeMutableBufferPointer<UInt8>
 	let scalar: UnsafeMutableBufferPointer<Float>
 	let vector: UnsafeMutableBufferPointer<float4>
 	let matrix: UnsafeMutableBufferPointer<float4x4>
@@ -23,6 +24,7 @@ internal struct Buffer {
 			mtl = nil
 			raw = NSData()
 		}
+		stream = UnsafeMutableBufferPointer<UInt8>(start: UnsafeMutablePointer<UInt8>(raw.bytes), count: raw.length)
 		scalar = UnsafeMutableBufferPointer<Float>(start: UnsafeMutablePointer<Float>(raw.bytes), count: raw.length/sizeof(Float))
 		vector = UnsafeMutableBufferPointer<float4>(start: UnsafeMutablePointer<float4>(raw.bytes), count: raw.length/sizeof(Float)/4)
 		matrix = UnsafeMutableBufferPointer<float4x4>(start: UnsafeMutablePointer<float4x4>(raw.bytes), count: raw.length/sizeof(Float)/16)
@@ -30,6 +32,7 @@ internal struct Buffer {
 	init( let raw ref: NSData ) {
 		mtl = nil
 		raw = NSData(data: ref)
+		stream = UnsafeMutableBufferPointer<UInt8>(start: UnsafeMutablePointer<UInt8>(raw.bytes), count: raw.length)
 		scalar = UnsafeMutableBufferPointer<Float>(start: UnsafeMutablePointer<Float>(raw.bytes), count: raw.length/sizeof(Float))
 		vector = UnsafeMutableBufferPointer<float4>(start: UnsafeMutablePointer<float4>(raw.bytes), count: raw.length/sizeof(Float)/4)
 		matrix = UnsafeMutableBufferPointer<float4x4>(start: UnsafeMutablePointer<float4x4>(raw.bytes), count: raw.length/sizeof(Float)/16)
--- C³/Error.swift
+++ C³/Error.swift
@@ -7,16 +7,25 @@
 //
 
 import Foundation
-enum MetalError: String, ErrorType {
+enum MetalError: String, ErrorType, CustomStringConvertible {
 	case DeviceNotFound = "Metal Device Not Found"
 	case LibraryNotAvailable = "Metal Library Not Available"
+	var description: String {
+		return rawValue
+	}
 }
-enum CoreDataError: String, ErrorType {
+enum CoreDataError: String, ErrorType, CustomStringConvertible {
 	case ModelNotFound = "Core Data Model Not Found"
 	case ModelNotAvailable = "Core Data Model Not Available"
+	var description: String {
+		return rawValue
+	}
 }
-enum SystemError: String, ErrorType {
+enum SystemError: String, ErrorType, CustomStringConvertible {
 	case RNGNotFound = "RNG device is not opened"
 	case FailObjectDecode = "Object is not correctly decoded"
 	case InvalidOperation = "Invalid operation"
+	var description: String {
+		return rawValue
+	}
 }
