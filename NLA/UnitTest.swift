ad3a65d95ade975cb7e2eb13b6934d7d695b4b2c
Kota Nakano <kotan.kn@me.com>
2016-07-20 21:46:15 +0900

    [bugfix] metal based normal rng

--- NLA/UnitTest.swift
+++ NLA/UnitTest.swift
@@ -10,6 +10,33 @@ import XCTest
 @testable import NLA
 
 class UnitTests: XCTestCase {
+	func testNormal() {
+		guard let device: MTLDevice = MTLCreateSystemDefaultDevice() else {
+			XCTFail("no device found")
+			return
+		}
+		guard let unit: Unit = try? Unit(device: device) else {
+			XCTFail("no unit created")
+			return
+		}
+		let rows: Int = 256
+		let cols: Int = 256
+		let U: la_object_t = la_splat_from_float(500.0, Unit.attr)
+		let S: la_object_t = la_splat_from_float(100.0, Unit.attr)
+		let mu: la_object_t = la_matrix_from_splat(U, la_count_t(rows), la_count_t(cols))
+		let sigma: la_object_t = la_matrix_from_splat(S, la_count_t(rows), la_count_t(cols))
+		let N: la_object_t = unit.normal(mu: mu, sigma: sigma)
+		let values: [Float] = [Float](count: rows*cols, repeatedValue: 0)
+		unit.join()
+		la_matrix_to_float_buffer(UnsafeMutablePointer<Float>(values), la_count_t(cols), N)
+		
+		let m: Float = values.reduce(0){$0+$1}/Float(rows*cols)
+		let s: Float = sqrtf(values.map{($0-m)*($0-m)}.reduce(0){$0+$1}/Float(rows*cols))
+		
+//		print(values)
+		print(m, s)
+		
+	}
 	func testSigmoid() {
 		do {
 			guard let device: MTLDevice = MTLCreateSystemDefaultDevice() else {
@@ -18,7 +45,7 @@ class UnitTests: XCTestCase {
 			}
 			let unit: Unit = try Unit(device: device)
 			let x: la_object_t = la_matrix_from_float_buffer(Array<Float>([-4.0,-2.0, 0.0, 2.0]), la_count_t(2), la_count_t(2), la_count_t(2), Unit.hint, Unit.attr)
-			let y: la_object_t = unit.sigmoid(x).0
+			let y: la_object_t = unit.sigmoid(x)
 			unit.join()
 			let v: [Float] = [0,0,0,0]
 			la_matrix_to_float_buffer(UnsafeMutablePointer<Float>(v), la_count_t(2), y)
