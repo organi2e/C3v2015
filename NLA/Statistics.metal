f97fcc32bd7fe839dc2abf2da732655c5c781bac
Kota Nakano <kotan.kn@me.com>
2016-07-20 21:46:15 +0900

    [bugfix] metal based normal rng

--- NLA/Statistics.metal
+++ NLA/Statistics.metal
@@ -10,19 +10,14 @@
 using namespace metal;
 float4 erf(float4);
 kernel void normal(device float4 * random [[buffer(0)]],
-				   device const uint * const noise [[buffer(3)]],
+				   device const uint * const noise [[buffer(1)]],
 				   uint const id [[thread_position_in_grid]]
 				   ){
-	float4 const n = unpack_unorm4x8_to_float(noise[id]) + 1/256.0;
+	float4 const n = ( unpack_unorm4x8_to_float ( noise[id] ) * 255.0 + 1.0 ) / 256.0;
 	float2 const c = cospi(2.0*n.xy);
 	float2 const s = sinpi(2.0*n.xy);
-	float2 const l = sqrt(-2.0*log(saturate(n.zw)));
-	
-	random[id].x = c.x*l.x;
-	random[id].y = s.x*l.x;
-	random[id].z = c.y*l.y;
-	random[id].w = s.y*l.y;
-	
+	float2 const l = sqrt(-2.0*log(n.zw));
+
 	random[id].xz = c;
 	random[id].yw = s;
 	random[id].xy *= l.x;
@@ -53,20 +48,20 @@ kernel void sigmoid(device float4 * const p [[ buffer(0) ]],
 					device const float4 * const x [[ buffer(1) ]],
 					uint const id [[thread_position_in_grid]]
 					) {
-	p[id] = tanh(x[id]);
+	p[id] = 0.5 + 0.5 * tanh(x[id]);
 }
 float4 erf(float4 z) {
-	float4 t = 1.0 / (1.0 + 0.5 * fabs(z));
+	float4 t = 1.0 / ( 1.0 + 0.5 * fabs(z) );
 	float4 ans = 1.0 - t * exp( -z*z -  1.26551223 +
-							   t * ( 1.00002368 +
-									t * ( 0.37409196 +
-										 t * ( 0.09678418 +
-											  t * (-0.18628806 +
-												   t * ( 0.27886807 +
-														t * (-1.13520398 +
-															 t * ( 1.48851587 +
-																  t * (-0.82215223 +
-																	   t * ( 0.17087277
-																			))))))))));
+	t * ( 1.00002368 +
+	t * ( 0.37409196 +
+	t * ( 0.09678418 +
+	t * (-0.18628806 +
+	t * ( 0.27886807 +
+	t * (-1.13520398 +
+	t * ( 1.48851587 +
+	t * (-0.82215223 +
+	t * ( 0.17087277
+	))))))))));
 	return sign(z) * ans;
 }
--- NLA/Unit.swift
+++ NLA/Unit.swift
@@ -14,6 +14,8 @@ public class Unit {
 	private let pipelines: Pipelines
 	
 	private struct Pipelines {
+		let pdf: MTLComputePipelineState
+		let cdf: MTLComputePipelineState
 		let sigmoid: MTLComputePipelineState
 		let normal: MTLComputePipelineState
 	}
@@ -35,6 +37,8 @@ public class Unit {
 			return try device.newComputePipelineStateWithFunction(function)
 		}
 		self.pipelines = Pipelines(
+			pdf: try pipeline("pdf"),
+			cdf: try pipeline("cdf"),
 			sigmoid: try pipeline("sigmoid"),
 			normal: try pipeline("normal")
 		)
@@ -44,7 +48,52 @@ public class Unit {
 	func step ( let x: la_object_t ) {
 		
 	}
-	func sigmoid ( let x: la_object_t ) -> ( la_object_t, dispatch_semaphore_t ) {
+	func pdf ( let x x: la_object_t, let u: la_object_t, let s: la_object_t ) -> la_object_t {
+		let rows: Int = Int(la_matrix_rows(x))
+		let cols: Int = Int(la_matrix_cols(x))
+		let cache: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>(malloc(sizeof(Float)*rows*cols))
+		
+		let command: MTLCommandBuffer = queue.commandBuffer()
+		let encoder: MTLComputeCommandEncoder = command.computeCommandEncoder()
+		
+		let ybuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
+		let xbuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
+		let ubuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
+		let sbuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
+		
+		let gather: dispatch_semaphore_t = dispatch_semaphore_create(0)
+		
+		Unit.async {
+			la_matrix_to_float_buffer(UnsafeMutablePointer<Float>(xbuf.contents()), la_count_t(cols), x)
+			la_matrix_to_float_buffer(UnsafeMutablePointer<Float>(ubuf.contents()), la_count_t(cols), u)
+			la_matrix_to_float_buffer(UnsafeMutablePointer<Float>(sbuf.contents()), la_count_t(cols), s)
+			dispatch_semaphore_signal(gather)
+		}
+		encoder.setComputePipelineState(pipelines.pdf)
+		encoder.setBuffer(ybuf, offset: 0, atIndex: 0)
+		encoder.setBuffer(xbuf, offset: 0, atIndex: 1)
+		encoder.setBuffer(ubuf, offset: 0, atIndex: 2)
+		encoder.setBuffer(sbuf, offset: 0, atIndex: 3)
+		encoder.setBytes([Float(0.5*M_2_SQRTPI*M_SQRT1_2)], length: sizeof(Float), atIndex: 4)
+		encoder.dispatchThreadgroups(MTLSize(width: rows*cols/4, height: 1, depth: 1), threadsPerThreadgroup: MTLSize(width: 1, height: 1, depth: 1))
+		encoder.endEncoding()
+		command.addScheduledHandler {(_)in
+			dispatch_semaphore_wait(gather, DISPATCH_TIME_FOREVER)
+		}
+		command.addCompletedHandler {(_)in
+			memcpy(cache, ybuf.contents(), ybuf.length)
+			ybuf.setPurgeableState(.Empty)
+			xbuf.setPurgeableState(.Empty)
+			ubuf.setPurgeableState(.Empty)
+			sbuf.setPurgeableState(.Empty)
+		}
+		command.commit()
+		return la_matrix_from_float_buffer_nocopy(cache, la_count_t(rows), la_count_t(cols), la_count_t(cols), Unit.hint, {free($0)}, Unit.attr)
+	}
+	func cdf ( let x: la_object_t ) -> la_object_t {
+		return x
+	}
+	func sigmoid ( let x: la_object_t ) -> la_object_t {
 		let rows: Int = Int(la_matrix_rows(x))
 		let cols: Int = Int(la_matrix_cols(x))
 		let cache: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>(malloc(sizeof(Float)*rows*cols))
@@ -52,7 +101,6 @@ public class Unit {
 		let xbuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
 		let ybuf: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
 		
-		let finish: dispatch_semaphore_t = dispatch_semaphore_create(0)
 		let gather: dispatch_semaphore_t = dispatch_semaphore_create(0)
 		
 		Unit.async {
@@ -71,48 +119,48 @@ public class Unit {
 		}
 		command.addCompletedHandler {(_)in
 			memcpy(cache, ybuf.contents(), ybuf.length)
-			dispatch_semaphore_signal(finish)
 			ybuf.setPurgeableState(.Empty)
 			xbuf.setPurgeableState(.Empty)
 		}
 		command.commit()
-		return (
-			la_sum(la_splat_from_float(0.5, Unit.attr), la_scale_with_float(la_matrix_from_float_buffer_nocopy(cache, la_count_t(rows), la_count_t(cols), la_count_t(cols), Unit.hint, {free($0)}, Unit.attr), 0.5)),
-			finish
-		)
+		return la_matrix_from_float_buffer_nocopy(cache, la_count_t(rows), la_count_t(cols), la_count_t(cols), Unit.hint, {free($0)}, Unit.attr)
 	}
-	func normal ( let rows: Int, let cols: Int ) -> ( la_object_t, dispatch_semaphore_t ) {
+	func normal ( let mu mu: la_object_t, let sigma: la_object_t ) -> la_object_t {
+		
+		assert(la_matrix_rows(mu)==la_matrix_rows(sigma))
+		assert(la_matrix_cols(mu)==la_matrix_cols(sigma))
+		
+		let rows: Int = Int(la_matrix_rows(mu))
+		let cols: Int = Int(la_matrix_cols(mu))
+		
 		let cache: UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>(malloc(sizeof(Float)*rows*cols))
 		
 		let result: MTLBuffer = device.newBufferWithLength(sizeof(Float)*rows*cols, options: .CPUCacheModeDefaultCache)
 		let random: MTLBuffer = device.newBufferWithLength(sizeof(UInt8)*rows*cols, options: .CPUCacheModeDefaultCache)
 		
-		let finish: dispatch_semaphore_t = dispatch_semaphore_create(0)
-		let gather: dispatch_semaphore_t = dispatch_semaphore_create(0)
-		
 		let command: MTLCommandBuffer = queue.commandBuffer()
 		let encoder: MTLComputeCommandEncoder = command.computeCommandEncoder()
 		
-		Unit.async {
-			arc4random_buf(random.contents(), random.length)
-			dispatch_semaphore_signal(gather)
-		}
+		let gather: dispatch_semaphore_t = dispatch_semaphore_create(0)
+		
+		//Unit.async {
+		arc4random_buf(random.contents(), random.length)
+		dispatch_semaphore_signal(gather)
+		//}
+		
 		encoder.setComputePipelineState(pipelines.normal)
 		encoder.setBuffer(result, offset: 0, atIndex: 0)
 		encoder.setBuffer(random, offset: 0, atIndex: 1)
 		encoder.dispatchThreadgroups(MTLSize(width: rows*cols/4, height: 1, depth: 1), threadsPerThreadgroup: MTLSize(width: 1, height: 1, depth: 1))
 		encoder.endEncoding()
-		command.addScheduledHandler {(_)in
-			dispatch_semaphore_wait(gather, DISPATCH_TIME_FOREVER)
-		}
 		command.addCompletedHandler {(_)in
 			memcpy(cache, result.contents(), sizeof(Float)*rows*cols)
-			dispatch_semaphore_signal(finish)
 			random.setPurgeableState(.Empty)
 			result.setPurgeableState(.Empty)
 		}
 		command.commit()
-		return (la_matrix_from_float_buffer_nocopy(cache, la_count_t(rows), la_count_t(cols), la_count_t(cols), Unit.hint, {free($0)}, Unit.attr), finish)
+		
+		return la_sum(mu, la_elementwise_product(sigma, la_matrix_from_float_buffer_nocopy(cache, la_count_t(rows), la_count_t(cols), la_count_t(cols), Unit.hint, {free($0)}, Unit.attr)))
 	}
 	func join() {
 		let command: MTLCommandBuffer = queue.commandBuffer()
