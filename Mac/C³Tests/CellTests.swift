817c82ff07549907220b04743c2138b425918013
Kota Nakano <Kota@Atelier.local>
2016-07-25 00:54:38 +0900

    [checkpoint] worked

--- Mac/C³Tests/CellTests.swift
+++ Mac/C³Tests/CellTests.swift
@@ -10,113 +10,35 @@ import XCTest
 @testable import C3
 
 class CellTests: XCTestCase {
-	static let key: Int = Int(arc4random())
+	static let keys: [Int] = [Int(arc4random()), Int(arc4random())]
 	static let value: [Float] = [arc4random(), arc4random(), arc4random(), arc4random()].map{Float($0)/Float(UInt32.max)}
-	func testSearch0() {
-		do {
-			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
-			let context: Context = try Context(storage: url)
-			if let cell: Cell = context.newCell(width: 4, label: "test\(CellTests.key)") {
-				
-				print("init \(CellTests.key)")
-				print("\(CellTests.value[0])")
-				print("\(CellTests.value[1])")
-				print("\(CellTests.value[2])")
-				print("\(CellTests.value[3])")
-				
-				cell[0] = CellTests.value[0]
-				cell[1] = CellTests.value[1]
-				cell[2] = CellTests.value[2]
-				cell[3] = CellTests.value[3]
-				
-				XCTAssert(cell[0]==CellTests.value[0])
-				XCTAssert(cell[1]==CellTests.value[1])
-				XCTAssert(cell[2]==CellTests.value[2])
-				XCTAssert(cell[3]==CellTests.value[3])
-
-				context.store() {(_)in
-					XCTFail()
-				}
-				print("done")
-				
-			} else {
-				XCTFail()
-			}
-		} catch let e {
-			XCTFail(String(e))
-		}
-	}
 	
-	func testSearch1() {
+	static let f: Bool = false
+	static let t: Bool = true
+	
+	let IS: [[Bool]] = [[f,t,f,f], [f,f,t,t], [f,f,t,f], [f,f,f,t]]
+	let OS: [[Bool]] = [[t,f,f,f], [f,t,f,f], [f,f,t,f], [f,f,f,t]]
+	
+	func test0Insert() {
 		do {
 			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
 			let context: Context = try Context(storage: url)
-			if let cell: Cell = context.searchCell(label: "test\(CellTests.key)").first {
-				
-				print("test1")
-				print("V:\(cell[0]) vs M:\(CellTests.value[0])")
-				print("V:\(cell[1]) vs M:\(CellTests.value[1])")
-				print("V:\(cell[2]) vs M:\(CellTests.value[2])")
-				print("V:\(cell[3]) vs M:\(CellTests.value[3])")
-				
-				XCTAssert(cell[0]==CellTests.value[0])
-				XCTAssert(cell[1]==CellTests.value[1])
-				XCTAssert(cell[2]==CellTests.value[2])
-				XCTAssert(cell[3]==CellTests.value[3])
-				
-				cell[0] = CellTests.value[3]
-				cell[1] = CellTests.value[2]
-				cell[2] = CellTests.value[1]
-				cell[3] = CellTests.value[0]
-				
-				print("V:\(cell[0]) vs M:\(CellTests.value[3])")
-				print("V:\(cell[1]) vs M:\(CellTests.value[2])")
-				print("V:\(cell[2]) vs M:\(CellTests.value[1])")
-				print("V:\(cell[3]) vs M:\(CellTests.value[0])")
-				
-				XCTAssert(cell[0]==CellTests.value[3])
-				XCTAssert(cell[1]==CellTests.value[2])
-				XCTAssert(cell[2]==CellTests.value[1])
-				XCTAssert(cell[3]==CellTests.value[0])
-				
-				print("STORE")
-				
+			if let
+				I: Cell = context.newCell(width: 4, label: "I"),
+				H: Cell = context.newCell(width: 4, label: "H", input: [I]),
+				O: Cell = context.newCell(width: 4, label: "O", input: [H])
+			{
+				I.oClear()
+				O.iClear()
+				I.active = [true, false, false, false]
+				print("try fire: \(O.active)")
+				print("test correct")
+				O.answer = [true, false, false, false]
+				I.correct(eps: 1/4.0)
 				context.store() {(_)in
 					XCTFail()
 				}
-			} else {
-				print(context.searchCell(label: "test\(CellTests.key)"))
-				XCTFail()
-			}
-		} catch let e {
-			XCTFail(String(e))
-		}
-	}
-	func testSearch2() {
-		do {
-			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
-			let context: Context = try Context(storage: url)
-			if let cell: Cell = context.searchCell(label: "test\(CellTests.key)").first {
 				
-				print("test2")
-				print("V:\(cell[0]) vs M:\(CellTests.value[3])")
-				print("V:\(cell[1]) vs M:\(CellTests.value[2])")
-				print("V:\(cell[2]) vs M:\(CellTests.value[1])")
-				print("V:\(cell[3]) vs M:\(CellTests.value[0])")
-				
-				XCTAssert(cell[0]==CellTests.value[3])
-				XCTAssert(cell[1]==CellTests.value[2])
-				XCTAssert(cell[2]==CellTests.value[1])
-				XCTAssert(cell[3]==CellTests.value[0])
-				
-				cell[0] = CellTests.value[3]
-				cell[1] = CellTests.value[2]
-				cell[2] = CellTests.value[1]
-				cell[3] = CellTests.value[0]
-				
-				context.store() {(_)in
-					XCTFail()
-				}
 			} else {
 				XCTFail()
 			}
@@ -124,5 +46,125 @@ class CellTests: XCTestCase {
 			XCTFail(String(e))
 		}
 	}
+	func test1Update() {
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let
+				I: Cell = context.searchCell(label: "I").last,
+				O: Cell = context.searchCell(label: "O").last
+			{
+				(0..<4096).forEach {
+					
+					I.oClear()
+					O.iClear()
+					
+					O.answer = OS[$0%4]
+					I.active = IS[$0%4]
+					
+					print("epoch \($0): \(O.active)")
+					I.correct(eps: 1/16.0)
+				}
+				context.commit()
+				context.store() {(_)in
+					XCTFail()
+				}
+				
+			} else {
+				XCTFail()
+			}
+		} catch let e {
+			XCTFail(String(e))
+		}
+	}
+	func test2Validation() {
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let
+				I: Cell = context.searchCell(label: "I").last,
+				O: Cell = context.searchCell(label: "O").last
+			{
+				(0..<4).forEach {
+					I.oClear()
+					O.iClear()
+					
+					I.active = IS[$0%4]
+					print("validate: \(O.active)")
+					
+					XCTAssert(O.active==OS[$0%4])
+				}
+				context.store() {(_)in
+					XCTFail()
+				}
+				
+			} else {
+				XCTFail()
+			}
+		} catch let e {
+			XCTFail(String(e))
+		}
+	}
+	/*
+	func test3Update() {
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let
+				I: Cell = context.searchCell(label: "I").last,
+				O: Cell = context.searchCell(label: "O").last
+			{
+				(0..<256).forEach {
+					print("epoch: \($0)")
+					
+					I.oClear()
+					O.iClear()
+					
+					O.answer = OS[$0%4]
+					I.active = IS[$0%4]
+					
+					print("epoch: \(O.active)")
+					I.correct(eps: 0.5)
+					
+				}
+				context.commit()
+				context.store() {(_)in
+					XCTFail()
+				}
+				
+			} else {
+				XCTFail()
+			}
+		} catch let e {
+			XCTFail(String(e))
+		}
+	}
+	func test4Validation() {
+		do {
+			let url: NSURL = try NSFileManager.defaultManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent("test.sqlite")
+			let context: Context = try Context(storage: url)
+			if let
+				I: Cell = context.searchCell(label: "I").last,
+				O: Cell = context.searchCell(label: "O").last
+			{
+				(0..<4).forEach {
+					I.oClear()
+					O.iClear()
+					
+					I.active = IS[$0%4]
+					
+					XCTAssert(O.active==OS[$0%4])
+				}
+				context.store() {(_)in
+					XCTFail()
+				}
+				
+			} else {
+				XCTFail()
+			}
+		} catch let e {
+			XCTFail(String(e))
+		}
+	}
+	*/
 }
-
