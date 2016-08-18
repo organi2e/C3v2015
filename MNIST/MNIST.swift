de59a2ab3b07525dfb89c36a97c0d067c093e956
Kota Nakano <Kota@Tools.local>
2016-08-18 14:29:36 +0900

    [feature] implement texture viewer

--- MNIST/MNIST.swift
+++ MNIST/MNIST.swift
@@ -9,14 +9,14 @@ import Metal
 import Foundation
 public class Image {
 	public let rows: Int
-	public let columns: Int
+	public let cols: Int
 	public let label: UInt8
-	public let pixel: [Float]
-	private init(let pixel: [Float], let label: UInt8, let rows: Int, let columns: Int) {
+	public let pixel: [UInt8]
+	private init(let pixel: [UInt8], let label: UInt8, let rows: Int, let cols: Int) {
 		self.label = label
 		self.pixel = pixel
 		self.rows = rows
-		self.columns = columns
+		self.cols = cols
 	}
 	private static func load(let image image: String, let label: String) -> [Image] {
 		var result: [Image] = []
@@ -32,17 +32,17 @@ public class Image {
 			let head: [Int] = headdata.toArray().map{Int(UInt32(bigEndian: $0))}
 			let length: Int = head[1]
 			let rows: Int = head[2]
-			let columns: Int = head[3]
+			let cols: Int = head[3]
 			let pixelsbody: [UInt8] = bodydata.toArray()
-			if length * rows * columns == pixelsbody.count, let data: NSData = dataFromBundle(label) where sizeof(UInt32) * 2 < data.length {
+			if length * rows * cols == pixelsbody.count, let data: NSData = dataFromBundle(label) where sizeof(UInt32) * 2 < data.length {
 				let(headdata, bodydata) = data.split(sizeof(UInt32) * 2)
 				let head: [Int] = headdata.toArray().map{Int(UInt32(bigEndian: $0))}
 				let length: Int = head[1]
 				let labelsbody: [UInt8] = bodydata.toArray()
 				if length == labelsbody.count {
-					let pixels: [[UInt8]] = pixelsbody.chunk(rows*columns)
+					let pixels: [[UInt8]] = pixelsbody.chunk(rows*cols)
 					let labels: [UInt8] = labelsbody
-					result = zip(pixels, labels).map{Image(pixel: $0.map{Float($0)}, label: $1, rows: rows, columns: columns)}
+					result = zip(pixels, labels).map{Image(pixel: $0, label: $1, rows: rows, cols: cols)}
 				}
 			}
 		}
