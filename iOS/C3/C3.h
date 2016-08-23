6a7076cd803be74ab6654025d71793804ad97202
Kota Nakano <Kota@lab-n678.team-lab.local>
2016-08-23 21:17:51 +0900

    [update] add iOS builds

---
+++ iOS/C3/C3.h
@@ -0,0 +1,19 @@
+//
+//  C3.h
+//  C3
+//
+//  Created by Kota Nakano on 8/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for C3.
+FOUNDATION_EXPORT double C3VersionNumber;
+
+//! Project version string for C3.
+FOUNDATION_EXPORT const unsigned char C3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <C3/PublicHeader.h>
+
+
