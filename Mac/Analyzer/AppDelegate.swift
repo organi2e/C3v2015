669282e825be44b82e87b7cac9e1ae63f43d7a32
Kota Nakano <Kota@Atelier.local>
2016-08-24 01:31:17 +0900

    [update] add analyzer/Mac

---
+++ Mac/Analyzer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Analyzer
+//
+//  Created by Kota Nakano on 8/24/16.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Mac/Analyzer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Analyzer
+//
+//  Created by Kota Nakano on 8/24/16.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
