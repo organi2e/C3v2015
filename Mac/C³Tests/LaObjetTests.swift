a7740a4b9192af779be4d6777c24fb7226c95410
Kota Nakano <kotan.kn@me.com>
2016-08-31 21:53:26 +0900

    [update] implement comparison system

--- Mac/C³Tests/LaObjetTests.swift
+++ Mac/C³Tests/LaObjetTests.swift
@@ -12,6 +12,13 @@ class LaObjetTests: XCTestCase {
 	var X: [Float] {
 		return [-14, 4]
 	}
+	var iter: Int {
+		return 180
+	}
+	var rate: Float {
+		return 0.01
+	}
+	
 	
 	func f(x: [Float]) -> Float {
 		return x[0]*x[0]/20 + x[1]*x[1]
@@ -24,11 +31,11 @@ class LaObjetTests: XCTestCase {
 	func testSGD() {
 		var x: [Float] = X
 		let fp = fopen("/tmp/SGD.raw", "wb")
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
-			x[0] -= g[0]*0.9
-			x[1] -= g[1]*0.9
+			x[0] -= g[0]*0.95
+			x[1] -= g[1]*0.95
 		}
 		print(x, f(x))
 		fclose(fp)
@@ -44,41 +51,41 @@ class LaObjetTests: XCTestCase {
 		let cg: GradientOptimizer = ConjugateGradient(dim: 2, type: .DY)
 		let fp = fopen("/tmp/Conjugate.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
 			let X = LaMatrice(x, rows: 2, cols: 1)
 			let h = cg.optimize(Δx: G, x: X).array
-			x[0] -= h[0]/2
-			x[1] -= h[1]/2
+			x[0] -= h[0]/4
+			x[1] -= h[1]/4
 		}
 		print(x, f(x))
 		fclose(fp)
 	}
 	
 	func testMomentum() {
-		let optimizer: GradientOptimizer = Momentum(dim: 2, α: 0.9)
+		let optimizer: GradientOptimizer = Momentum(dim: 2, α: 0.5)
 		let fp = fopen("/tmp/Momentum.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
 			let X = LaMatrice(x, rows: 2, cols: 1)
 			let h = optimizer.optimize(Δx: G, x: X).array
-			x[0] -= h[0]/2
-			x[1] -= h[1]/2
+			x[0] -= h[0]*rate
+			x[1] -= h[1]*rate
 		}
 		print(x, f(x))
 		fclose(fp)
 	}
 	
 	func testAdam() {
-		let optimizer: GradientOptimizer = Adam(dim: 2, α: 0.5)
+		let optimizer: GradientOptimizer = Adam(dim: 2, α: rate)
 		let fp = fopen("/tmp/Adam.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
@@ -92,10 +99,10 @@ class LaObjetTests: XCTestCase {
 	}
 	
 	func testSMORMS3() {
-		let optimizer: GradientOptimizer = SMORMS3(dim: 2)
+		let optimizer: GradientOptimizer = SMORMS3(dim: 2, α: rate)
 		let fp = fopen("/tmp/SMORMS3.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
@@ -112,7 +119,7 @@ class LaObjetTests: XCTestCase {
 		let optimizer: GradientOptimizer = RMSprop(dim: 2, γ: 0.5)
 		let fp = fopen("/tmp/RMSprop.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
@@ -129,7 +136,7 @@ class LaObjetTests: XCTestCase {
 		let qn: GradientOptimizer = QuasiNewton(dim: 2, type: .SR1)
 		let fp = fopen("/tmp/QuasiNewton.raw", "wb")
 		var x: [Float] = X
-		for _ in 0...40 {
+		for _ in 0...iter {
 			fwrite(x, sizeof(Float), 2, fp)
 			let g = J(x)
 			let G = LaMatrice(g, rows: 2, cols: 1)
@@ -147,17 +154,16 @@ class LaObjetTests: XCTestCase {
 from matplotlib.pylab import *
 figure('render')
 clf()
-X, Y = meshgrid(arange(-10,10,0.1), arange(-10,10,0.1))
+X, Y = meshgrid(arange(-20,20,0.1), arange(-20,20,0.1))
 Z = sqrt((X**2)/20 + Y**2)
-contour(X, Y, Z, 16, colors='k')
-for k in ['SGD', 'Adam', 'Conjugate', 'SMORMS3', 'QuasiNewton','Momentum']:
+contour(X, Y, Z, 16, colors='gray')
+for k in ['SGD', 'Momentum', 'Adam', 'RMSProp', 'SMORMS3', 'QuasiNewton', 'Conjugate']:
 	x = fromfile('/tmp/%s.raw'%k, 'float32')
-	plot(x[0::2],x[1::2],label=k)
-	rc('font',family='Times New Roman')
+	plot(x[0:2*i:2],x[1:2*i:2],label=k)
 
 legend(loc=1)
-xlim([-10,10])
-ylim([-10,10])
+xlim([-15,15])
+ylim([-15,15])
 draw()
 show(block=False)
 */
@@ -165,21 +171,19 @@ show(block=False)
 /*
 from matplotlib.pylab import *
 figure('render')
-for i in range(1,20):
+for i in range(1,180):
 	clf()
-	X, Y = meshgrid(arange(-10,10,0.1), arange(-10,10,0.1))
+	X, Y = meshgrid(arange(-20,20,0.1), arange(-20,20,0.1))
 	Z = sqrt((X**2)/20 + Y**2)
 	contour(X, Y, Z, 16, colors='k')
 	for k in ['SGD', 'Momentum', 'Adam', 'RMSProp', 'SMORMS3', 'QuasiNewton', 'Conjugate']:
 		x = fromfile('/tmp/%s.raw'%k, 'float32')
-		print(1)
 		plot(x[0:2*i:2],x[1:2*i:2],label=k)
-		print(2)
 	rc('font',family='Times New Roman')
 	legend(loc=1)
-	xlim([-10,10])
-	ylim([-10,10])
+	xlim([-20,20])
+	ylim([-20,20])
 	draw()
 	show(block=False)
-	savefig('%02d.png'%i)
+	savefig('%03d.png'%i)
 */
